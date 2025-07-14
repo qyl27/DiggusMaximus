b428e218c6520cddf9e0a37af86c3228ba296941
秋雨落 <i@rain.cx>
2025-07-14 21:14:20 +0800

    Rollback changes about startBlock determining.
    Signed-off-by: 秋雨落 <i@rain.cx>

--- src/main/java/net/kyrptonaught/diggusmaximus/ModNetworking.java
+++ src/main/java/net/kyrptonaught/diggusmaximus/ModNetworking.java
@@ -1,12 +1,8 @@
 package net.kyrptonaught.diggusmaximus;
 
 import dev.architectury.injectables.annotations.ExpectPlatform;
-import net.kyrptonaught.diggusmaximus.excavate.Shape;
 import net.kyrptonaught.diggusmaximus.networking.ExcavateFailedPacket;
 import net.kyrptonaught.diggusmaximus.networking.ExcavatePacket;
-import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 
 public class ModNetworking {
