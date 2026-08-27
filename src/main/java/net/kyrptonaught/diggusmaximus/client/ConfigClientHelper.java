a7df759efd108accdbaa79fc6a22ffbdf27169cb
秋雨落 <i@rain.cx>
2026-08-27 22:03:43 +0800

    feat: upgrade to 26.2.
    Signed-off-by: 秋雨落 <i@rain.cx>

--- src/main/java/net/kyrptonaught/diggusmaximus/client/ConfigClientHelper.java
+++ src/main/java/net/kyrptonaught/diggusmaximus/client/ConfigClientHelper.java
@@ -2,17 +2,10 @@ package net.kyrptonaught.diggusmaximus.client;
 
 import me.shedaniel.autoconfig.AutoConfigClient;
 import net.kyrptonaught.diggusmaximus.config.ModConfig;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.Screen;
 
 public class ConfigClientHelper {
     public static Screen getConfigScreen(Screen parent) {
         return AutoConfigClient.getConfigScreen(ModConfig.class, parent).get();
-    }
-
-    public static void showConfigScreen(Minecraft mc) {
-        var parent = mc.screen;
-        var screen = getConfigScreen(parent);
-        mc.setScreen(screen);
     }
 }
